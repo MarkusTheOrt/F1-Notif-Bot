a103394d3201355f28d4cce2a1703883abcd6247
Markus Ort <markus@ort.dev>
2024-06-21 14:32:44 +0200

    feat: remove Shuttle.rs as dependency

--- src/main.rs
+++ src/main.rs
@@ -13,20 +13,24 @@ use serenity::{client::ClientBuilder, prelude::GatewayIntents};
 
 use crate::{bot::Bot, util::handle_config_error};
 
-#[shuttle_runtime::main]
-async fn main() -> shuttle_serenity::ShuttleSerenity {
+#[tokio::main]
+async fn main() -> Result<(), String> {
     let mut config = match File::open("./config/config.toml") {
         Ok(config) => config,
         Err(why) => handle_config_error(why),
     };
     let mut string = "".to_owned();
     if let Err(why) = config.read_to_string(&mut string) {
-        return Err(anyhow!("Error reading config file: \n\t`{why}`").into());
+        return Err(
+            anyhow!("Error reading config file: \n\t`{why}`").to_string()
+        );
     }
     let config = match toml::from_str::<Config>(string.as_str()) {
         Ok(config) => config,
         Err(why) => {
-            return Err(anyhow!("Error parsing config file:\n\t`{why}`").into())
+            return Err(
+                anyhow!("Error parsing config file:\n\t`{why}`").to_string()
+            )
         },
     };
 
@@ -38,21 +42,23 @@ async fn main() -> shuttle_serenity::ShuttleSerenity {
     let database = match MySqlPool::connect_with(db_options).await {
         Ok(db) => db,
         Err(why) => {
-            return Err(anyhow!("Error creating db client:\n\t`{why}`").into())
+            return Err(
+                anyhow!("Error creating db client:\n\t`{why}`").to_string()
+            )
         },
     };
 
     let Ok(mut cat_video) = File::open("./config/cats.mp4") else {
-        return Err(anyhow!("Error opening the cat.").into());
+        return Err(anyhow!("Error opening the cat.").to_string());
     };
 
     let Ok(cat_meta) = cat_video.metadata() else {
-        return Err(anyhow!("No metadata on the cat.").into());
+        return Err(anyhow!("No metadata on the cat.").to_string());
     };
     let mut cat_data = Vec::with_capacity(cat_meta.len() as usize);
 
     let Ok(_) = cat_video.read_to_end(&mut cat_data) else {
-        return Err(anyhow!("Can't see the cats insides.").into());
+        return Err(anyhow!("Can't see the cats insides.").to_string());
     };
 
     let config = Box::leak(Box::new(config));
@@ -64,7 +70,7 @@ async fn main() -> shuttle_serenity::ShuttleSerenity {
         cat: cat_data.leak(),
     };
 
-    let client = match ClientBuilder::new(
+    let mut client = match ClientBuilder::new(
         &bot.config.discord.bot_token,
         GatewayIntents::non_privileged(),
     )
@@ -73,11 +79,10 @@ async fn main() -> shuttle_serenity::ShuttleSerenity {
     {
         Ok(client) => client,
         Err(why) => {
-            return Err(
-                anyhow!("Error creating discord client: \n\t`{why}`").into()
-            )
+            return Err(anyhow!("Error creating discord client: \n\t`{why}`")
+                .to_string())
         },
     };
 
-    Ok(client.into())
+    client.start_autosharded().await.map_err(|f| f.to_string())
 }
